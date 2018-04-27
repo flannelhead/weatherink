35984ec4df67d01f44cdfe6dc3b3c9bfdbd98ee6
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2018-04-27 13:22:43 +0300

    Add some fonts stuff

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # weatherink
 E-ink weather display implemented on ESP8266
+
+# Dependencies
+
+Arduino ESP8266 platform 2.4.1 or greater with the following libraries:
+
+* Adafruit GFX Library
+* WiFiManager
+* Json Streaming Parser
+* [GxEPD](https://github.com/ZinggJM/GxEPD) (must be pulled from git to libraries folder)
+
+# Acknowledgements
+
+`meteocons.h` has been rendered from the [Meteocons font](http://www.alessioatzeni.com/meteocons/)
+by Alessio Atzeni.
--- weatherink.ino
+++ weatherink.ino
@@ -9,15 +9,31 @@
 #include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
 #include <GxIO/GxIO.cpp>
 
+#include "meteocons.h"
+
 GxIO_Class io(SPI, D8, D3, D4); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
 GxEPD_Class display(io, D4, D2);
 
 const int CONFIG_PIN = D1;
+const int DISPLAY_WIDTH = 250;
+const int DISPLAY_HEIGHT = 122;
+
+
+void drawChar(Adafruit_GFX &gfx, int cx, int cy, char c)
+{
+	int16_t x, y;
+	uint16_t w, h;
+	gfx.getTextBounds(&c, 0, 0, &x, &y, &w, &h);
+	int dx = x + w/2;
+	int dy = y + h/2;
+	gfx.drawChar(cx - dx, cy - dy, c, GxEPD_BLACK, GxEPD_WHITE, 1);
+}
 
 void setup()
 {
-  display.init();
-  /*
+	Serial.begin(115200);
+
+	/*
 	pinMode(CONFIG_PIN, INPUT_PULLUP);
 
 	if (digitalRead(CONFIG_PIN))
@@ -25,7 +41,14 @@ void setup()
 		WiFiManager wifiManager;
 		wifiManager.startConfigPortal("weatherink");
 	}
- */
+	*/
+
+	display.init();
+	display.setRotation(1);
+	display.fillScreen(GxEPD_WHITE);
+	display.setFont(&meteocons_webfont48pt7b);
+	drawChar(display, DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2, 'A');
+	display.update();
 }
 
 void loop()
