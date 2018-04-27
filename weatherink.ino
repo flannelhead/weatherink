06046d468cfafcd3f42080bbfc7181bd3733f185
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2018-04-27 14:05:19 +0300

    Fix memory bug

--- weatherink.ino
+++ weatherink.ino
@@ -29,7 +29,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "meteocons.h"
 
-GxIO_Class io(SPI, D8, D3, D4); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
+GxIO_Class io(SPI, D8, D3, D4);
 GxEPD_Class display(io, D4, D2);
 
 const int CONFIG_PIN = D1;
@@ -39,29 +39,31 @@ const int DISPLAY_HEIGHT = 122;
 
 void drawChar(Adafruit_GFX &gfx, int cx, int cy, char c)
 {
+	char buf[] = {c, '\0'};
 	int16_t x, y;
 	uint16_t w, h;
-	gfx.getTextBounds(&c, 0, 0, &x, &y, &w, &h);
+	gfx.getTextBounds(buf, 0, 0, &x, &y, &w, &h);
 	int dx = x + w/2;
 	int dy = y + h/2;
+	Serial.println(x);
+	Serial.println(y);
+	Serial.println(w);
+	Serial.println(h);
 	gfx.drawChar(cx - dx, cy - dy, c, GxEPD_BLACK, GxEPD_WHITE, 1);
 }
 
 void setup()
 {
-	Serial.begin(115200);
+	display.init();
 
-	/*
 	pinMode(CONFIG_PIN, INPUT_PULLUP);
-
-	if (digitalRead(CONFIG_PIN))
+	delay(10);
+	if (!digitalRead(CONFIG_PIN))
 	{
 		WiFiManager wifiManager;
 		wifiManager.startConfigPortal("weatherink");
 	}
-	*/
 
-	display.init();
 	display.setRotation(1);
 	display.fillScreen(GxEPD_WHITE);
 	display.setFont(&meteocons_webfont48pt7b);
@@ -71,5 +73,5 @@ void setup()
 
 void loop()
 {
-
+	delay(5000);
 }
